5f75f7fd942fd90183453235fcfb8f1eb466aea4
ShallowFeather <74778927+ShallowFeather@users.noreply.github.com>
2021-05-10 03:49:12 +0800

    Update monsters.rs

--- src/monsters.rs
+++ src/monsters.rs
@@ -1,64 +1,87 @@
 use bevy::prelude::*;
 use core::time::*;
+use bevy::ecs::system::Command;
+use core::f32::consts::PI;
 
 pub struct Monster;
 
-pub struct SpawnTimer(Timer);
+pub struct SpawnTimer(pub Timer);
 
 pub fn spawn_monster_up(mut commands: Commands,
                         materials: Res<crate::Materials>,
-) {
-
-    let transform = Transform::from_translation(Vec3::new( 0.,450. , 1.));
-    commands
-        .spawn_bundle(SpriteBundle{
-            material: materials.monster_material.clone(),
-            sprite: Sprite::new(Vec2::new(100., 100.)),
-            transform,
-            ..Default::default()
-        })
-        .insert(Monster);
+                        time: Res<Time>,
+                        mut timer: ResMut<SpawnTimer>)
+{
+    if timer.0.tick(time.delta()).just_finished() {
+        let transform = Transform::from_translation(Vec3::new(0., 450., 1.));
+        commands
+            .spawn_bundle(SpriteBundle {
+                material: materials.monster_material.clone(),
+                sprite: Sprite::new(Vec2::new(100., 100.)),
+                transform,
+                ..Default::default()
+            })
+            .insert(Monster);
+    }
 }
 
-pub fn spawn_monster_down(mut commands: Commands, materials: Res<crate::Materials>) {
-    let transform = Transform::from_translation(Vec3::new(0.,-450. , 1.));
-    commands
-        .spawn_bundle(SpriteBundle{
-            material: materials.monster_material.clone(),
-            sprite: Sprite::new(Vec2::new(100., 100.)),
-            transform,
-            ..Default::default()
-        })
-        .insert(Monster);
+pub fn spawn_monster_down(mut commands: Commands,
+                          materials: Res<crate::Materials>,
+                          time: Res<Time>,
+                          mut timer: ResMut<SpawnTimer>)
+{
+    if timer.0.tick(time.delta()).just_finished() {
+        let transform = Transform::from_translation(Vec3::new(0., -450., 1.));
+        commands
+            .spawn_bundle(SpriteBundle {
+                material: materials.monster_material.clone(),
+                sprite: Sprite::new(Vec2::new(100., 100.)),
+                transform,
+                ..Default::default()
+            })
+            .insert(Monster);
+    }
 }
 
-pub fn spawn_monster_left(mut commands: Commands, materials: Res<crate::Materials>) {
-    let transform = Transform::from_translation(Vec3::new( -700.,0. , 1.));
-    commands
-        .spawn_bundle(SpriteBundle{
-            material: materials.monster_material.clone(),
-            sprite: Sprite::new(Vec2::new(100., 100.)),
-            transform,
-            ..Default::default()
-        })
-        .insert(Monster);
+pub fn spawn_monster_left(mut commands: Commands,
+                          materials: Res<crate::Materials>,
+                          time: Res<Time>,
+                          mut timer: ResMut<SpawnTimer>)
+{
+    if timer.0.tick(time.delta()).just_finished() {
+        let transform = Transform::from_translation(Vec3::new(-700., 0., 1.));
+        commands
+            .spawn_bundle(SpriteBundle {
+                material: materials.monster_material.clone(),
+                sprite: Sprite::new(Vec2::new(100., 100.)),
+                transform,
+                ..Default::default()
+            })
+            .insert(Monster);
+    }
 }
 
-pub fn spawn_monster_right(mut commands: Commands, materials: Res<crate::Materials>) {
-    let transform = Transform::from_translation(Vec3::new( 700.,0. , 1.));
-    commands
-        .spawn_bundle(SpriteBundle{
-            material: materials.monster_material.clone(),
-            sprite: Sprite::new(Vec2::new(100., 100.)),
-            transform,
-            ..Default::default()
-        })
-        .insert(Monster);
+pub fn spawn_monster_right(mut commands: Commands,
+                           materials: Res<crate::Materials>,
+                           time: Res<Time>,
+                           mut timer: ResMut<SpawnTimer>)
+{
+    if timer.0.tick(time.delta()).just_finished() {
+        let transform = Transform::from_translation(Vec3::new(700., 0., 1.));
+        commands
+            .spawn_bundle(SpriteBundle {
+                material: materials.monster_material.clone(),
+                sprite: Sprite::new(Vec2::new(100., 100.)),
+                transform,
+                ..Default::default()
+            })
+            .insert(Monster);
+    }
 }
 
 pub fn move_monster_up(time: Res<Time>, mut query: Query<(&mut Transform, &Monster)>){
     for (mut transform, _monster) in query.iter_mut() {
-        if transform.translation.y > 200. {
+        if transform.translation.y > 100. {
             transform.translation.y -= time.delta_seconds() * 200.;
         }
     }
@@ -66,25 +89,44 @@ pub fn move_monster_up(time: Res<Time>, mut query: Query<(&mut Transform, &Monst
 
 pub fn move_monster_down(time: Res<Time>, mut query: Query<(&mut Transform, &Monster)>){
     for (mut transform, _monster) in query.iter_mut() {
-        if transform.translation.y < -200. {
+        if transform.translation.y < -100. {
             transform.translation.y += time.delta_seconds() * 200.;
+        }
+    }
+}
+
+pub fn move_monster_right(time: Res<Time>, mut query: Query<(&mut Transform, &Monster)>){ for (mut transform, _monster) in query.iter_mut() {
+        if transform.translation.x > 100. {
+            transform.translation.x -= time.delta_seconds() * 200.;
         }
     }
 }
 
 pub fn move_monster_left(time: Res<Time>, mut query: Query<(&mut Transform, &Monster)>){
     for (mut transform, _monster) in query.iter_mut() {
-        if transform.translation.x > 200. {
-            transform.translation.x -= time.delta_seconds() * 200.;
-        }
-    }
-}
-
-pub fn move_monster_right(time: Res<Time>, mut query: Query<(&mut Transform, &Monster)>){
-    for (mut transform, _monster) in query.iter_mut() {
-        if transform.translation.x < (-200.) {
+        if transform.translation.x < (-100.) {
             transform.translation.x += time.delta_seconds() * 200.;
         }
     }
 }
 
+pub fn despawns_monster_right(mut commands: Commands,
+                              keyboard_input: Res<Input<KeyCode>>,
+                              mut positions: Query<(Entity, &mut Transform), With<crate::character::Character>>)
+{
+    for mut transform in positions.iter_mut() {
+        transform.
+        let posx = transform.translation.x;
+        let posy = transform.translation.y;
+        let mut path = transform.rotation.z;
+        if  posx <= 150. && 100. <= posx
+            && path == (1. * PI)
+            && keyboard_input.just_pressed(KeyCode::O)
+        {
+            commands.entity(entity).despawn();
+        }
+        else if posx > 0. && posx < (110.) && posy == 0. {
+            commands.entity(entity).despawn();
+        }
+    }
+}
